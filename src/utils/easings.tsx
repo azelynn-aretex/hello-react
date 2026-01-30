28a5d92ad10018c7afb762eb96979236378417c8
azelynn-aretex <azelynn.aretex@gmail.com>
2026-01-30 16:02:19 +1100

    Add easings

---
+++ src/utils/easings.tsx
@@ -0,0 +1,5 @@
+import type { Easing } from "motion";
+
+export const easeInQuint: Easing = [0.64, 0, 0.78, 0];
+export const easeOutQuint: Easing = [0.22, 1, 0.36, 1];
+export const easeInOutQuint: Easing = [0.83, 0, 0.17, 1];
